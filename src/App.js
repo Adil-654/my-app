9ce4ed8e4d255afff775eacdae2e5b1ae1004788
Adil-654 <adilahmadshah7860@gmail.com>
2024-01-25 11:07:17 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useState } from 'react';
 
+import React from 'react'
+
+const App=()=>{
+  const [name,setName]=useState("")
+  const [fullName,setFullName]=useState()
+  const input=(e)=>{
+    console.log(e.target.value);
+    setName(e.target.value)
+  }
+  const onSubmit=()=>{
+    setFullName({name}) 
+  }
+ return( <>
+<div>
+  <h1>Hello {fullName}</h1>  
+  <input type="text" placeholder='Enter your name' 
+  onChange={input}
+  />
+<button onClick={onSubmit}>click me</button>
+</div>
+  
+  </>);
+};
 export default App;
